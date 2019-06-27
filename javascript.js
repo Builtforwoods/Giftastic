128a9c4a28f2d0864346e66c2e2041bf44f2d37d
Rodolfo Morales <rodolfomlphx@gmail.com>
2019-06-27 15:57:55 -0700

    added the javascript oh my gosh it was so hard and me darn button won't fire even if I use a class

--- javascript.js
+++ javascript.js
@@ -1,1 +1,36 @@
-var topics = ["American Farming", "Mexican Ranching", "Catechism", "Soren Kierkegaard", "Restaurant", "Overwatch","Mexico","Arizona","High School"]
+var topics = ["American Farming", "Mexican Ranching", "Catechism", "Soren Kierkegaard", "Restaurant", "Overwatch","Mexico","Arizona","High School"];
+// this button isn't working idk y it's making me mad
+
+$(".btn btn-primary").on("click",function(){
+    const ourValue = $(this).val().trim();
+    const queryURL = "https://api.giphy.com/v1/gifs/random?api_key=FQMJH5AjTQFOuKEYl9lAX2mdth9ktPZj&q=" + ourValue;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      .then(function(response) {
+          const imageUrl = response.data.image_original_url;
+
+          const gifImage = $("<img>");
+
+          gifImage.attr("src", imageUrl);
+          
+          $(#images").prepend(catImage);
+      });
+   
+
+    
+
+$(".gif").on("click", function() {
+    
+    const state = $(this).attr("data-state");
+    
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
